5f5bbbe49b5360ae26762f8f29b39829d2b7cf0a
Jacob Arriola <jacob.arriola@gmail.com>
2014-10-01 19:28:37 -0700

    made blog post title h1 instead of h2

--- single.php
+++ single.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-			<h2><?php the_title(); ?></h2>
+			<h1><?php the_title(); ?></h1>
 			<span class="glyphicon glyphicon-calendar"></span> <span class="bpost-date"><?php the_date(); ?></span>
         </header>
         <section>
